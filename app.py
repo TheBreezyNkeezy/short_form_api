169a2f3212a7b42b31839542fe2e836eef57c3be
Josh Nkoy <joshnk27@aol.com>
2022-11-12 17:57:13 -0700

    problems importing before rename

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
-import sys
+from short-form-api.scraper import parse_website
 from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route("/hello", methods=["GET"])
 def hello_world():
-    if request.method == "GET":
-        return "<p>Hello, world!</p>", 201
+    return "<p>Hello, world!</p>", 201
+
+@app.route("/parse", methods=["POST"])
+def parse():
+    url=request.json["url"]
+    return parse_website(url)
 
 if __name__ == "__main__":
     app.run(debug=True)
